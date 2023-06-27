3351af99499bbff7c35b81e255defaa5645a9287
mahfuz-1994 <117646198+mahfuz-1994@users.noreply.github.com>
2023-06-27 10:23:05 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Welcome to Clover Infotech from devops team! ";
 	}
 
 	public static void main(String[] args) {
